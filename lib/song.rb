6a40a481355a3cc74c0e2d6775c2c2d0fe77c82a
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-03 16:03:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,22 @@
+require 'pry'
 
+class Song
+  attr_accessor :name, :artist_name
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def create
+    song = self.new
+    @@all << song
+    self.all
+  end
+
+end
 #binding.pry
